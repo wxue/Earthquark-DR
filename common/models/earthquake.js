932fca20e0180165dac773e22b99ba530c39f9c5
wxue <wxue2012@gmail.com>
2018-01-12 02:00:57 -0800

    middle step -- restore data by regions

--- common/models/earthquake.js
+++ common/models/earthquake.js
@@ -1,3 +1,5 @@
+var async = require('async');
+
 module.exports = function(Earthquake) {
     /*
    *  get most dangerous regions
@@ -8,14 +10,56 @@ module.exports = function(Earthquake) {
     if (!count) count = 10;
     if (!days) days = 30;
     if (!region_type) region_type = 'area';
-
     var time_offset = days*24*60*60*1000; // days*hours*minutes*seconds*milliseconds
 
-    filters.limit = count;
+    var regions = new Object();
+
     filters.where = {and:[]};
     filters.where.and.push({createTime:{gte: Date.now()-time_offset}});
     return Earthquake.find({filters}).then(function(records) {
-      console.log(records);
+      // console.log(records);
+
+      /*
+      *  restore data by regions
+      */ 
+      if (region_type == 'area') {
+        async.each(records, function(record, callback){
+
+          if (record.energyRelease) {
+            if (regions.hasOwnProperty(record.area)) {
+              regions[record.area].count += 1; 
+              regions[record.area].totalEnergyRelease += (record.energyRelease); 
+            }
+            else {
+              regions[record.area]={
+                count:1, 
+                totalEnergyRelease:record.energyRelease
+              };
+            };
+          }
+
+        });
+      }
+      else if (region_type == 'timezone') {
+        async.each(records, function(record, callback){
+          
+          if (record.energyRelease) {
+            if (regions.hasOwnProperty(record.timeZone)) {
+              regions[record.timeZone].count += 1; 
+              regions[record.timeZone].totalEnergyRelease += record.energyRelease; 
+            }
+            else {
+              regions[record.timeZone]={
+                count:1, 
+                totalEnergyRelease:record.energyRelease
+              };
+            };
+          }
+
+        });
+      };
+      console.log(regions);
+
       // return result;
     });
 
